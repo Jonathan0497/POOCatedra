5dbce3b7079c20988417fa9ede84067d2e93ab21
Jonathan0497 <jonathanmendoza2001.jm@gmail.com>
2023-09-08 01:13:33 -0600

    se creo el menú de inicio
    Carlos aporto con ideas y imágenes, sin el no podría haberlo logrado
    
    Co-Authored-By: Carislost <135508493+Carislost@users.noreply.github.com>

--- ProyectoCatedraPOO/src/Inicio.java
+++ ProyectoCatedraPOO/src/Inicio.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import javax.swing.GroupLayout;
 /*
- * Created by JFormDesigner on Thu Sep 07 20:17:51 CST 2023
+ * Created by JFormDesigner on Fri Sep 08 00:42:40 CST 2023
  */
 
 
@@ -18,20 +18,71 @@ public class Inicio extends JFrame {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Jonathan Mendoza
         menuBar1 = new JMenuBar();
+        btn_inicio = new JMenuItem();
+        menuBar2 = new JMenuBar();
+        btn_cinematografia = new JMenu();
+        btn_categoria = new JMenuItem();
+        btn_formato = new JMenuItem();
+        btn_peliculas = new JMenuItem();
+        btn_usuario = new JMenuItem();
+        btn_miUsuario = new JMenuItem();
 
         //======== this ========
         var contentPane = getContentPane();
+
+        //======== menuBar1 ========
+        {
+
+            //---- btn_inicio ----
+            btn_inicio.setText("Inicio");
+            btn_inicio.setIcon(new ImageIcon(getClass().getResource("/icon/casa.png")));
+            menuBar1.add(btn_inicio);
+
+            //======== menuBar2 ========
+            {
+
+                //======== btn_cinematografia ========
+                {
+                    btn_cinematografia.setText("Cinematografia");
+                    btn_cinematografia.setIcon(new ImageIcon(getClass().getResource("/icon/camara-de-video.png")));
+
+                    //---- btn_categoria ----
+                    btn_categoria.setText("categorias");
+                    btn_cinematografia.add(btn_categoria);
+
+                    //---- btn_formato ----
+                    btn_formato.setText("formato");
+                    btn_cinematografia.add(btn_formato);
+
+                    //---- btn_peliculas ----
+                    btn_peliculas.setText("peliculas");
+                    btn_cinematografia.add(btn_peliculas);
+                }
+                menuBar2.add(btn_cinematografia);
+
+                //---- btn_usuario ----
+                btn_usuario.setText("Usuario");
+                btn_usuario.setIcon(new ImageIcon(getClass().getResource("/icon/editar.png")));
+                menuBar2.add(btn_usuario);
+            }
+            menuBar1.add(menuBar2);
+
+            //---- btn_miUsuario ----
+            btn_miUsuario.setText("Mi Usuario");
+            btn_miUsuario.setIcon(new ImageIcon(getClass().getResource("/icon/usuario_1.png")));
+            menuBar1.add(btn_miUsuario);
+        }
         setJMenuBar(menuBar1);
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 683, Short.MAX_VALUE)
+                .addGap(0, 573, Short.MAX_VALUE)
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 510, Short.MAX_VALUE)
+                .addGap(0, 501, Short.MAX_VALUE)
         );
         pack();
         setLocationRelativeTo(getOwner());
@@ -41,5 +92,13 @@ public class Inicio extends JFrame {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Jonathan Mendoza
     private JMenuBar menuBar1;
+    private JMenuItem btn_inicio;
+    private JMenuBar menuBar2;
+    private JMenu btn_cinematografia;
+    private JMenuItem btn_categoria;
+    private JMenuItem btn_formato;
+    private JMenuItem btn_peliculas;
+    private JMenuItem btn_usuario;
+    private JMenuItem btn_miUsuario;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
