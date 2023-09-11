988605753b5ec86d054b6595c5473ce1f8227ae0
Jonathan0497 <jonathanmendoza2001.jm@gmail.com>
2023-09-10 23:25:54 -0600

    Crud Categoria
    se añadio el crud categoria completamente funcional pero falta validaciones

--- ProyectoCatedraPOO/src/Inicio.java
+++ ProyectoCatedraPOO/src/Inicio.java
@@ -1,8 +1,12 @@
+<<<<<<< Updated upstream
 import java.awt.*;
+=======
+import java.awt.event.*;
+>>>>>>> Stashed changes
 import javax.swing.*;
 import javax.swing.GroupLayout;
 /*
- * Created by JFormDesigner on Fri Sep 08 00:42:40 CST 2023
+ * Created by JFormDesigner on Thu Sep 07 20:17:51 CST 2023
  */
 
 
@@ -15,6 +19,19 @@ public class Inicio extends JFrame {
         initComponents();
     }
 
+    private void btn_categoria(ActionEvent e) {
+        CategoriaForm c = new CategoriaForm(this);
+        c.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Inicio().setVisible(true);
+            }
+        });
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Jonathan Mendoza
@@ -25,6 +42,7 @@ public class Inicio extends JFrame {
         btn_categoria = new JMenuItem();
         btn_formato = new JMenuItem();
         btn_peliculas = new JMenuItem();
+        btn_funcion = new JMenuItem();
         btn_usuario = new JMenuItem();
         btn_miUsuario = new JMenuItem();
 
@@ -48,17 +66,26 @@ public class Inicio extends JFrame {
                     btn_cinematografia.setIcon(new ImageIcon(getClass().getResource("/icon/camara-de-video.png")));
 
                     //---- btn_categoria ----
+<<<<<<< Updated upstream
                     btn_categoria.setText("categorias");
                     btn_categoria.setPreferredSize(new Dimension(125, 19));
+=======
+                    btn_categoria.setText("Categorias");
+                    btn_categoria.addActionListener(e -> btn_categoria(e));
+>>>>>>> Stashed changes
                     btn_cinematografia.add(btn_categoria);
 
                     //---- btn_formato ----
-                    btn_formato.setText("formato");
+                    btn_formato.setText("Formato");
                     btn_cinematografia.add(btn_formato);
 
                     //---- btn_peliculas ----
-                    btn_peliculas.setText("peliculas");
+                    btn_peliculas.setText("Peliculas");
                     btn_cinematografia.add(btn_peliculas);
+
+                    //---- btn_funcion ----
+                    btn_funcion.setText("Funci\u00f3n");
+                    btn_cinematografia.add(btn_funcion);
                 }
                 menuBar2.add(btn_cinematografia);
 
@@ -80,11 +107,11 @@ public class Inicio extends JFrame {
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 573, Short.MAX_VALUE)
+                .addGap(0, 583, Short.MAX_VALUE)
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 501, Short.MAX_VALUE)
+                .addGap(0, 516, Short.MAX_VALUE)
         );
         pack();
         setLocationRelativeTo(getOwner());
@@ -100,6 +127,7 @@ public class Inicio extends JFrame {
     private JMenuItem btn_categoria;
     private JMenuItem btn_formato;
     private JMenuItem btn_peliculas;
+    private JMenuItem btn_funcion;
     private JMenuItem btn_usuario;
     private JMenuItem btn_miUsuario;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
