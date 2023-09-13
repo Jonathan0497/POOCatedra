6ec01f38115e1d5d72cd812849e68fc08673f5a7
Carislost <villaloboscarlos213@gmail.com>
2023-09-13 17:11:45 -0600

    Creacion Form Sucursales

---
+++ ProyectoCatedraPOO/src/SucursalesForm.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+/*
+ * Created by JFormDesigner on Wed Sep 13 17:10:23 CST 2023
+ */
+
+
+
+/**
+ * @author Carlos
+ */
+public class SucursalesForm extends JDialog {
+    public SucursalesForm(Window owner) {
+        super(owner);
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Jonathan Mendoza
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Jonathan Mendoza
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
